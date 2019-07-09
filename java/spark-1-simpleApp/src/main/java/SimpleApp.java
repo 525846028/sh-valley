2e2b4a54fad6193d360608d893a697fc16753e40
Green Wang <cocowool@gmail.com>
2019-07-09 21:37:59 +0800

    fix FilterFunction

--- java/spark-1-simpleApp/src/main/java/SimpleApp.java
+++ java/spark-1-simpleApp/src/main/java/SimpleApp.java
@@ -1,4 +1,9 @@
+
+/**
+ * SimpleApp.java
+ */
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.api.java.function.FilterFunction;
 import org.apache.spark.sql.Dataset;
 
 public class SimpleApp {
@@ -8,10 +13,16 @@ public class SimpleApp {
         SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
         Dataset<String> logData = spark.read().textFile(logFile).cache();
 
-        long numNos = logData.filter(s -> s.contains("no")).count();
-        long numYes = logData.filter(s -> s.contains("yes")).count();
+        long numNos = logData.filter((FilterFunction<String>) x -> x.contains("no")).count();
+        // long numNos = logData.filter(new FilterFunction<String>(){
+        //     public boolean call(String x){
+        //         return x.contains("no");
+        //     }
+        // }).count();
 
+        long numYes = logData.filter((FilterFunction<String>) s->s.contains("yes")).count();
         System.out.println("Lines with no : " + numNos + ", with yes : " + numYes);
+
 
         spark.stop();
     }
