0a2a5d1b2da57ca4074145937ce3f168eebe7f7e
Green Wang <cocowool@qq.com>
2019-09-04 15:21:20 +0800

    test slf4j

---
+++ java/java-log4j/src/Main.java
@@ -0,0 +1,9 @@
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main(){
+    public static void main(String args[]){
+        System.out.println("Log4j example project");
+    }
+}
