95a5ee540986d8dd0a0a1da59fcccb0e59723d7c
Green Wang <cocowool@qq.com>
2019-09-04 17:56:38 +0800

    add baseline project

---
+++ java/java-baseline/src/main/java/com/edulinks/baseline/Baseline.java
@@ -0,0 +1,10 @@
+package com.edulinks.baseline;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Baseline {
+    public static void main(String[] args){
+        System.out.println("Baseline Project start.");
+    }
+}
