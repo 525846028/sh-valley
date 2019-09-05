c5390c9e3ddcf864579bb1a7f5aa98ed2cb3dfcf
Green Wang <cocowool@qq.com>
2019-09-05 10:55:46 +0800

    use log4j

--- java/java-baseline/src/main/java/com/edulinks/Baseline.java
+++ java/java-baseline/src/main/java/com/edulinks/Baseline.java
@@ -1,10 +1,15 @@
 package com.edulinks;
 
+import org.apache.log4j.Logger;
 // import org.slf4j.Logger;
 // import org.slf4j.LoggerFactory;
 
 public class Baseline {
+    private static Logger logger = Logger.getLogger(Baseline.class);
     public static void main(String[] args){
-        System.out.println("Baseline Project start.");
+        System.out.println(System.getProperty("java.class.path"));
+        logger.debug("Baseline Project start.");
+
+        System.out.println("Welcome use baseline project");
     }
 }
