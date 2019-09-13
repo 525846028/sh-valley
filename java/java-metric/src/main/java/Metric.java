220a518bd1234269fb1de9adccc711fb9436c0c8
Green Wang <cocowool@gmail.com>
2019-09-13 23:09:47 +0800

    test yammer metric

--- java/java-metric/src/main/java/Metric.java
+++ java/java-metric/src/main/java/Metric.java
@@ -1,10 +1,14 @@
 // //Metric examples by shiqiang
-// import GaugeExample;
+import GaugeExample;
+import CounterExample;
 
-// public class Metric {
-//     public void main(String[] args){
-//         GaugeExample ge = new GaugeExample();
+public class Metric {
+    public static void main(String[] args){
+        
+        System.out.println("Welcome use metrics example!");
+        // GaugeExample ge = new GaugeExample();
+        // ge.main();
 
-//         // ge.main();
-//     }
-// }
+        CounterExample ce = new CounterExample();
+    }
+}
