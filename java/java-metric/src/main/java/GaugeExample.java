86529a46e8245b1c6c0b0d11cb7f42084fc5aa47
Green Wang <cocowool@gmail.com>
2019-09-13 22:47:09 +0800

    modify metric example

---
+++ java/java-metric/src/main/java/GaugeExample.java
@@ -0,0 +1,31 @@
+// package main.java;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.Gauge;
+import com.yammer.metrics.reporting.ConsoleReporter;
+
+public class GaugeExample {
+    private List<String> stringList = new LinkedList<String>();
+    Gauge<Integer> gauge = Metrics.newGauge(GaugeExample.class, "list-size-gauge", new Gauge<Integer>(){
+        @Override
+        public Integer value(){
+            return stringList.size();
+        }
+    });
+
+    public void inputElement(String input){
+        stringList.add(input);
+    }
+
+    public static void main(String[] args) throws InterruptedException{
+        ConsoleReporter.enable(1, TimeUnit.SECONDS);
+        GaugeExample ge = new GaugeExample();
+        for(int i = 0; i < 10; i++){
+            ge.inputElement(String.valueOf(i));
+            Thread.sleep(1000);
+        }
+    }
+}
---
+++ java/java-metric/src/main/java/Metric.java
@@ -0,0 +1,10 @@
+// //Metric examples by shiqiang
+// import GaugeExample;
+
+// public class Metric {
+//     public void main(String[] args){
+//         GaugeExample ge = new GaugeExample();
+
+//         // ge.main();
+//     }
+// }
