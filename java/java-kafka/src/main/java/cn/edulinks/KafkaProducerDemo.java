093f58e2eba80774a189c722c9abd0d99bdf6100
Green Wang <cocowool@qq.com>
2019-09-17 16:35:03 +0800

    test local produce

--- java/java-kafka/src/main/java/cn/edulinks/KafkaProducerDemo.java
+++ java/java-kafka/src/main/java/cn/edulinks/KafkaProducerDemo.java
@@ -8,7 +8,7 @@ import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 public class KafkaProducerDemo {
-    private final static String TOPIC = "test";
+    private final static String TOPIC = "tst";
     private final static String BOOTSTRAP_SERVERS = "localhost:9092";
 
     public void run(){
@@ -21,5 +21,24 @@ public class KafkaProducerDemo {
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         Producer<Long, String> producer = new KafkaProducer<>(props);
+        long time = System.currentTimeMillis();
+
+        try {
+            for (long index = time; index < time + 100; index++){
+                final ProducerRecord<Long, String> record = new ProducerRecord<>(TOPIC, index, "Hello i'am " + index);
+                RecordMetadata metadata = producer.send(record).get();
+                
+                long elapsedTime = System.currentTimeMillis() - time;
+                System.out.printf("sent record(key=%s value=%s) " +
+                              "meta(partition=%d, offset=%d) time=%d\n",
+                      record.key(), record.value(), metadata.partition(),
+                      metadata.offset(), elapsedTime);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            producer.flush();
+            producer.close();
+        }
     }
 }
