ad9bfdcebbbba0249e1a95eafcea44367b33a782
Green Wang <cocowool@qq.com>
2019-09-18 19:17:08 +0800

    add param

--- java/java-kafka/src/main/java/cn/edulinks/Kafka.java
+++ java/java-kafka/src/main/java/cn/edulinks/Kafka.java
@@ -1,19 +1,51 @@
 package cn.edulinks;
 
+import java.text.ParseException;
+
+import org.apache.commons.cli.*;
+
 public class Kafka {
     public static void main(String[] args){
         System.out.println("Kafka Example start.");
 
-        // KafkaProducerDemo kp = new KafkaProducerDemo();
-        // kp.run();
+        //解析命令行参数, -type producer/consumer
+        Option opt1 = new Option("t", "type", true, "Choose Kafka Type");
+        opt1.setRequired(false);
 
-        // kp.async_send();
+        Options options = new Options();
+        options.addOption(opt1);
+
+        CommandLine cli = null;
+        CommandLineParser cliParser = new DefaultParser();
+        HelpFormatter helpFormatter = new HelpFormatter();
 
         try {
-            KafkaConsumerDemo kc = new KafkaConsumerDemo();
-            kc.run();    
+            cli = cliParser.parse(options, args);
         }catch(Exception e){
+            helpFormatter.printHelp(">>>>>> kafka demo options", options);
             e.printStackTrace();
         }
+
+        if(cli.hasOption("t")){
+            String t = cli.getOptionValue("t", "producer");
+            switch (t) {
+                case "consumer":
+                    try {
+                        KafkaConsumerDemo kc = new KafkaConsumerDemo();
+                        kc.run();    
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }                            
+                    break;
+                case "producer":
+                default:
+                    KafkaProducerDemo kp = new KafkaProducerDemo();
+                    kp.run();
+
+                    kp.async_send();
+                    break;
+            }
+        }
+
     }
 }
