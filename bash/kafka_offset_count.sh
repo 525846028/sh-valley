ed6be044824682ff345434a368ea8b4c9063db32
Green Wang <cocowool@qq.com>
2019-09-26 08:51:33 +0800

    add kafka consume count bash

--- bash/kafka_offset_count.sh
+++ bash/kafka_offset_count.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+#检查参数的数量
+if [ $# -ne 2 ]
+then
+    echo "Usage: ./kafka_offset_count.sh topic brokerlist"
+    exit 1
+fi
+
+if [ -n "$1" ]
+then
+    TOPIC=$1
+else
+    echo "Please input the topic name"
+    exit 1
+fi
+
+if [ -n "$2" ]
+then
+    BROKERLIST=$2
+else
+    echo "Please input the broker list"
+    exit 1
+fi
+
 while :
 do
-    ~/Projects/tools/kafka_2.11-2.1.0/bin/kafka-run-class.sh kafka.tools.GetOffsetShell --broker-list localhost:9092 --topic test
+    ~/Projects/tools/kafka_2.11-2.1.0/bin/kafka-run-class.sh kafka.tools.GetOffsetShell --broker-list $BROKERLIST --topic $TOPIC
     echo "-----------------"
     sleep 5
 done
