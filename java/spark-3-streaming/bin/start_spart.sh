97c9b50df4566ea29fa9357b0940f545ca8c4a2b
Green Wang <cocowool@qq.com>
2019-10-01 17:06:32 +0800

    add spark submit shell

---
+++ java/spark-3-streaming/bin/start_spart.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SPARK_HOME=/Users/rousseau/Projects/tools/spark-2.4.1-bin-hadoop2.7
+APP_HOME=/Users/rousseau/Projects/sh-valley/java/spark-4-kafka
+PID_NAME=StreamingProject
+MAIN_NAME=StreamingProject
+SPARK_LIB=$APP_HOME/lib
+APP_JAR=/Users/rousseau/Projects/sh-valley/java/spark-3-streaming/target/streaming-project-1.0.jar
+
+# 使用前需使用nc命令在另一个Term窗口执行 nc -lk 3333 命令
+$SPARK_HOME/bin/spark-submit \
+--conf spark.executor.extraClassPath=$SPARK_LIB \
+--conf spark.driver.extraClassPath=$SPARK_LIB \
+--class StreamingProject $APP_JAR localhost 3333
+
