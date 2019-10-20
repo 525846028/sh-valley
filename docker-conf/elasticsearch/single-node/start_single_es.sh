864a09bbb36bf93207513720427e2baf50eea193
Green Wang <cocowool@gmail.com>
2019-10-20 22:48:29 +0800

    add cluster config

--- docker-conf/elasticsearch/single-node/start_single_es.sh
+++ docker-conf/elasticsearch/single-node/start_single_es.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name es-single
+docker run -e ES_JAVA_OPTS="-Xms256m -Xmx256m" --name es-single -p 9200:9200 -p 9300:9300 -v /Users/shiqiang/Projects/sh-valley/docker-conf/elasticsearch/single-node:/usr/share/elasticsearch/config/elasticsearch.yml -d
