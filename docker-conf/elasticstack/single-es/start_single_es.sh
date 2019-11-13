30bde328c943fc0c891da11660a9f5c8cee9d870
Green Wang <cocowool@qq.com>
2019-11-13 08:26:20 +0800

    modify docker start shell

--- docker-conf/elasticstack/single-es/start_single_es.sh
+++ docker-conf/elasticstack/single-es/start_single_es.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -e ES_JAVA_OPTS="-Xms256m -Xmx256m" --name es-single -p 9200:9200 -p 9300:9300 -v /Users/shiqiang/Projects/sh-valley/docker-conf/elasticsearch/single-node/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml -d docker.elastic.co/elasticsearch/elasticsearch:6.6.2
+docker run -e ES_JAVA_OPTS="-Xms256m -Xmx256m" --name es-single -p 9200:9200 -p 9300:9300 -v /Users/shiqiang/Projects/sh-valley/docker-conf/elasticstack/single-es/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml -d docker.elastic.co/elasticsearch/elasticsearch:6.3.1
