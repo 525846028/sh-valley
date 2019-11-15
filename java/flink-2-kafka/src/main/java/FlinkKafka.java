fe88db3b43cb742f5845482052ae0ff3a4a5eb5b
Green Wang <cocowool@qq.com>
2019-11-15 19:18:53 +0800

    modify code

--- java/flink-2-kafka/src/main/java/FlinkKafka.java
+++ java/flink-2-kafka/src/main/java/FlinkKafka.java
@@ -1,9 +1,11 @@
 import java.util.Properties;
 
-import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.streaming.api.TimeCharacteristic;
-import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.utils.ParameterTool;
+// import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
 
@@ -12,21 +14,25 @@ import java.util.Map;
 
 public class FlinkKafka {
     public static void main(String[] args) throws Exception {
-        Properties properties = new Properties();
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        properties.setProperty("bootstrap.server", "192.168.65.2:9092");
-        properties.setProperty("zookeeper.connect", "192.168.65.2:2181");
-        properties.setProperty("group.id", "flink_kafka_consumer");
-        properties.setProperty("enable.auto.commit", "true");
-        properties.setProperty("auto.commit.interval.ms", "5000");
+        Map properties = new HashMap();
+
+        properties.put("bootstrap.server", "192.168.65.2:9092");
+        properties.put("zookeeper.connect", "192.168.65.2:2181");
+        properties.put("group.id", "flink_kafka_consumer");
+        properties.put("enable.auto.commit", "true");
+        properties.put("auto.commit.interval.ms", "5000");
+        properties.put("topic", "test_flink_input");
+
+        ParameterTool parameterTool = ParameterTool.fromMap(properties);
 
         FlinkKafkaConsumer010 consumer010 = new FlinkKafkaConsumer010(
-            "test_flink_input",
+            parameterTool.getRequired("topic"),
             new SimpleStringSchema(),
-            properties
+            parameterTool.getProperties()
         );
 
-        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         DataStream<String> ds = env.addSource(consumer010);
 
         // consumer010.assignTimestampsAndWatermarks(new CustomWatermarkEmitter());
